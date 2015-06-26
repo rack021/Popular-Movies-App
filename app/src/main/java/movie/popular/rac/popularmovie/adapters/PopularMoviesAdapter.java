c78f5bff0851047521f0c2f920686709adcd2d0d
rac <rack021@gmail.com>
2015-06-26 14:47:31 +0530

    update

--- app/src/main/java/movie/popular/rac/popularmovie/adapters/PopularMoviesAdapter.java
+++ app/src/main/java/movie/popular/rac/popularmovie/adapters/PopularMoviesAdapter.java
@@ -1,36 +1,65 @@
 package movie.popular.rac.popularmovie.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import movie.popular.rac.popularmovie.R;
+import movie.popular.rac.popularmovie.models.PopularMovieModel;
 
 /**
  * Created by User on 6/25/2015.
  */
 public class PopularMoviesAdapter extends BaseAdapter {
     private Context mContext;
-    public PopularMoviesAdapter(Context c){
+    private List<PopularMovieModel> mpopularMovieList;
+    public PopularMoviesAdapter(Context c, List<PopularMovieModel> popularMovieList){
         mContext = c;
+        mpopularMovieList = popularMovieList;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return mpopularMovieList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mpopularMovieList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
+    }
+
+    class ViewHolder {
+        ImageView thumb;
+        public ViewHolder(View v){
+            thumb = (ImageView)v.findViewById(R.id.poster);
+        }
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View row = convertView;
+        ViewHolder holder = null;
+        if(row == null){
+            LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.popularmovie_movie_grid_item,parent, false);
+            holder = new ViewHolder(row);
+            row.setTag(holder);
+        }else{
+            holder = (ViewHolder)row.getTag();
+        }
+        Picasso.with(mContext).load("http://image.tmdb.org/t/p/w185/"+ mpopularMovieList.get(position).poster_path).into(holder.thumb);
+        return row;
     }
 }
---
+++ app/src/main/java/movie/popular/rac/popularmovie/models/PopularMovieModel.java
@@ -0,0 +1,10 @@
+package movie.popular.rac.popularmovie.models;
+
+/**
+ * Created by User on 6/26/2015.
+ */
+
+public class PopularMovieModel {
+    public String title;
+    public String poster_path;
+}
